833b007e11af0aa826cdbef4220187d923507d81
Fleuv <fleuv@live.com>
2020-02-24 11:42:47 +0100

    hello world

---
+++ plugins/essif-lab/essif-lab.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: eSSIF-Lab
+ * Plugin URI: https://github.com/LSVH/hbo-ict-inno-s2-tno
+ * Description: The purpose of the eSSIF-Lab is to specify, develop and validate technological and non-technological means that support people, businesses and governments to think about, design and operate their (information) processes and (electronically) conduct business transactions with one another.
+ * Version: 1.0
+ * Author: Duur Klop, Luuk van Houdt, Ruben Sikkens, Ufuk Altinçöp en Weis Mateen
+ */
+echo "Hello world";
