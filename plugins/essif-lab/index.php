edf454c02ec24ef58abbc98c34c7a11e9be8bce3
Fleuv <fleuv@live.com>
2020-04-02 17:36:30 +0200

    Added TextDomain to plugin data

--- plugins/essif-lab/index.php
+++ plugins/essif-lab/index.php
@@ -3,8 +3,9 @@
  * Plugin Name: eSSIF-Lab
  * Plugin URI: https://github.com/LSVH/hbo-ict-inno-s2-tno
  * Description: The purpose of the eSSIF-Lab is to specify, develop and validate technological and non-technological means that support people, businesses and governments to think about, design and operate their (information) processes and (electronically) conduct business transactions with one another.
- * Version: 1.0
  * Author: Duur Klop, Luuk van Houdt, Ruben Sikkens, Ufuk Altinçöp en Weis Mateen
+ * TextDomain: tno-essif-lab
+ * Version: 1.0
  */
 
 defined( 'ABSPATH' ) or die();
