14cfdffb6fa18470d6766bbf691c17fb54dddaa8
Fleuv <fleuv@live.com>
2020-04-02 17:57:38 +0200

    Added RegistersPostTypes interface

--- plugins/essif-lab/src/Application/Controllers/Admin.php
+++ plugins/essif-lab/src/Application/Controllers/Admin.php
@@ -5,14 +5,22 @@ namespace TNO\EssifLab\Controllers;
 defined('ABSPATH') or die();
 
 use TNO\EssifLab\Abstracts\Controller;
+use TNO\EssifLab\Interfaces\RegistersPostTypes;
 
-class Admin extends Controller
-{
+class Admin extends Controller implements RegistersPostTypes {
 	public function getActions(): array {
+		$this->addAction('init', $this, 'registerPostTypes');
+
 		return $this->actions;
 	}
 
 	public function getFilters(): array {
 		return $this->filters;
+	}
+
+	public function registerPostTypes(): void {
+		register_post_type($this->getDomain().'_validation_policy', [
+			'label' => '',
+		]);
 	}
 }
