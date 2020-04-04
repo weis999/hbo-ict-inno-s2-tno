b2bd5e1cbf1d317928d0bb622d65b2acda84d283
Fleuv <fleuv@live.com>
2020-04-04 09:42:34 +0200

    Added workflow for managing hooks, crud functions should be added here

---
+++ plugins/essif-lab/src/Application/Workflows/ManageHooks.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TNO\EssifLab\Application\Workflows;
+
+use TNO\EssifLab\Contracts\Abstracts\Core;
+
+class ManageHooks extends Core {
+	protected $post_id;
+
+	protected $post;
+
+	public function __construct($pluginData, $requestData) {
+		parent::__construct($pluginData);
+		[$post_id, $post] = $requestData;
+		$this->post_id = $post_id;
+		$this->post = $post;
+	}
+
+	public function execute($request): void {
+		[$action, $payload] = $request;
+		if (method_exists($this, $action)) {
+			$this->{$action}($payload);
+		}
+	}
+
+	public function add() {
+		// TODO: add a hook to a validation policy
+	}
+
+	public function edit($hook) {
+		// TODO: edit a hook of a validation policy
+	}
+
+	public function delete($id) {
+		// TODO: delete a hook of a validation policy
+	}
+}
