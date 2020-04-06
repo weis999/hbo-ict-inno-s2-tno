67dda26d53af3fc17e7e2db6f5e2d9f7d58bb397
weis999 <31774334+weis999@users.noreply.github.com>
2020-04-06 22:49:25 +0200

    Create ExperimentTests.py

---
+++ docker/ExperimentTests.py
@@ -0,0 +1,17 @@
+__author__ = 'TN0'
+
+import unittest
+from Experiment import Greeter
+
+class MyTestCase(unittest.TestCase):
+    def test_default_greeting_set(self):
+        greeter = Greeter()
+        # this test will fail until you change the Greeter to return this expected message
+        self.assertEqual(greeter.message, 'Hello world!')
+        # this test will fail if the Greeter does not return anything
+        self.assertIsNotNone(greeter.message)
+        # this test will fail if the Greeter's instance is not the same as that of the class 'str'
+        self.assertIsInstance(greeter.message, str)
+
+if __name__ == '__main__':
+    unittest.main()
